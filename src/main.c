5623fadcb19f9cb490721b5285a510151b7a55ca
vassilios <vassilioslg@outlook.com>
2026-06-14 16:45:32 +0100

    extended the type 'builtin' to search for executable files via PATH

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#include <unistd.h> 
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -21,11 +22,38 @@ int main(int argc, char *argv[]) {
       else if (strncmp(input, "type ", 5) == 0) {char *arg = input +5;
       if (!strcmp(arg, "exit") || !strcmp(arg, "type") || !strcmp(arg, "echo")) 
       {printf("%s is a shell builtin\n", arg);} 
-      else{printf("%s: not found\n", arg);} }
-      
-      else {printf("%s: command not found\n", input);} 
-     
+
+      else { 
+        
+         
+        char path_copy[1000]; 
+        strcpy(path_copy, getenv("PATH")); 
+
+        char *dir = strtok(path_copy, ":"); 
+        int found = 0;
+
+        while (dir != NULL) {
+          char full_path[500];
+          snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);  
+        
+        if (access(full_path, X_OK) == 0) {
+        printf("%s is %s\n", arg, full_path);
+        found = 1; 
+        break; 
+        }
+
+        dir = strtok(NULL, ":");
+      }
+
+      if (!found){
+        printf("%s: not found\n", arg);
+      }
     }
+  }
+      
+  else {printf("%s: command not found\n", input);} }
+     
+    
 
   return 0;
 }
